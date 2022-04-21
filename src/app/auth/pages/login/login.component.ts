20f48a4e24788862120b9503bc327caa77a3cb0f
Antonio Andrade <antoniomaresmares001gmail.com>
2022-04-20 23:22:43 -0500

    peticion post correcta con usuario registrado

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,16 +14,25 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   miFormulario:FormGroup = this._fb.group({
-    email:['test1@test.com', [ Validators.required, Validators.email ]],
-    password: ['123456', [Validators.required, Validators.minLength(6) ]]
+    email:['tonytolver@gmail.com', [ Validators.required, Validators.email ]],
+    password: ['121212', [Validators.required, Validators.minLength(6) ]]
   })
 
   constructor( private _fb:FormBuilder,
-               private _router:Router ) { }
+               private _router:Router,
+               private _authService:AuthService ) { }
 
   login(){
-    console.log( this.miFormulario.value )
-    this._router.navigateByUrl('/dashboard')
+    // extraer datos del formulario 
+    const { email, password } = this.miFormulario.value
+
+    this._authService.login( email, password )
+        .subscribe( resp =>{
+          console.log( resp )
+        });
+    
+
+    // this._router.navigateByUrl('/dashboard')
   }
 
 }
